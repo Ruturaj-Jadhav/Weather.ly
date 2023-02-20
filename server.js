25e78d3a170f707909bdf1abd0f7ca5fcde9f60f
Rutz13 <ruturajjadhav456@gmail.com>
2023-02-20 19:22:40 +0530

    Css added

--- server.js
+++ server.js
@@ -3,34 +3,36 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const https = require('https');
 const app = express();
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
+var city = "City";
+var temp = "Temperature";
+var description = "Description";
 app.post('/' , function(req,res){
   var name = req.body.city;
+  city = name;
+  console.log(name);
   const url1 = 'https://api.openweathermap.org/geo/1.0/direct?q='+name+'&appid=1fe231302abc885776886d9f9d695588';
-  const url = 'https://api.openweathermap.org/data/2.5/weather?lat=53.958332&lon=-1.080278&appid=1fe231302abc885776886d9f9d695588&units=metric';
   https.get(url1, function(response){
     response.on("data" , function(data){
       const coordinatesData = JSON.parse(data);
+      console.log(coordinatesData);
 
       const lat = coordinatesData[0].lat;
 
       const lon = coordinatesData[0].lon;
       const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=1fe231302abc885776886d9f9d695588&units=metric';
-      //console.log(coordinatesData);
       https.get(url,function(response1){
         response1.on("data" , function(data){
           const weatherData = JSON.parse(data);
-          const temp = weatherData.main.temp;
+          const Temp = weatherData.main.temp;
           const icon = weatherData.weather[0].icon;
-          const description = weatherData.weather[0].description;
+          const Description = weatherData.weather[0].description;
           const imgURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-          res.write("<h1>Weather check</h1> <br>");
-          res.write("City : "+name+"<br>");
-          res.write("Description : "+description +"<br>");
-          res.write("Current temperature : " + temp + " degree celcius.");
-          res.write("<img src=" + imgURL + ">");
-          res.send();
+          temp = Temp;
+          description = Description;
+          res.redirect('/');
         });
 
       });
@@ -39,10 +41,11 @@ app.post('/' , function(req,res){
 
   });
 
+
 });
 
 app.get('/' , function(req , res){
-res.sendFile(__dirname+"/index.html");
+res.render('index' , {City :city , info : description , Temp : temp });
 });
 
 app.listen(3000, function(){
